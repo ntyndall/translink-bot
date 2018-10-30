321b019d7e64e4ff9420136fc44e7feefec517d5
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-10-30 10:34:38 +0000

    Parse XML first.

---
+++ R/query_live.R
@@ -0,0 +1,28 @@
+#' @title Query Live Data
+#' 
+#' @export
+
+
+query_live <- function(stationCode) {
+  details <- paste0("https://apis.opendatani.gov.uk/translink/", stationCode, ".xml") %>%
+    httr::GET() %>%
+    `[[`("content") %>%
+    rawToChar()
+  
+  # Convert to xml and get all services first
+  services <- details %>%
+    xml2::read_xml() %>%
+    xml2::xml_find_all(".//Service")
+  
+  # Get calling points
+  callingpoints <- lapply(
+    X = services, 
+    FUN = function(x) x %>% xml2::xml_find_all(".//CallingPoint") %>% xml2::as_list() %>% lapply(attributes)
+  )
+  
+  # Get the remainder of tags
+  rest <- lapply(
+    X = ".//" %>% paste0(translink.bot::allowed_tags()),
+    FUN = function(y) services %>% xml2::xml_find_first(y) %>% xml2::as_list() %>% lapply(attributes)
+  )
+}
