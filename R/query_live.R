3ffc0c537e277c9e897a74fa7cbcc8117c071f4c
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-10-30 10:57:30 +0000

    Convert calling points to a list of data frames

--- R/query_live.R
+++ R/query_live.R
@@ -14,15 +14,29 @@ query_live <- function(stationCode) {
     xml2::read_xml() %>%
     xml2::xml_find_all(".//Service")
   
-  # Get calling points
+  # Get calling points as a list of data frames
   callingpoints <- lapply(
     X = services, 
     FUN = function(x) x %>% xml2::xml_find_all(".//CallingPoint") %>% xml2::as_list() %>% lapply(attributes)
+  ) %>% lapply(
+    FUN = function(x) {
+      if (x %>% length %>% `!=`(0)) {
+        x %>% lapply(as.data.frame) %>% purrr::reduce(rbind) 
+      } else {
+        NULL
+      }
+    }
   )
   
   # Get the remainder of tags
   rest <- lapply(
     X = ".//" %>% paste0(translink.bot::allowed_tags()),
     FUN = function(y) services %>% xml2::xml_find_first(y) %>% xml2::as_list() %>% lapply(attributes)
+  )
+  
+  # Now convert to a data frame
+  myresults <- lapply(
+    X = rest,
+    FUN = function(x) x %>% lapply(as.data.frame) %>% purrr::reduce(rbind)
   )
 }
