867ec721af1ec3716cdb0098cc482fe0b4e82557
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-07 13:43:36 +0000

    Check favourites + retrieval.

--- tests/testthat/test-parse_message.R
+++ tests/testthat/test-parse_message.R
@@ -9,7 +9,7 @@ event <- list(
 # Set up REDIS connection
 dbr <- redux::hiredis()
 
-test_that("Return a simple `to` statement.", {
+test_that("Check `to` statement.", {
 
   # Add text
   event$text <- "<> a to b"
@@ -24,7 +24,7 @@ test_that("Return a simple `to` statement.", {
 
 })
 
-test_that("Make sure favourites can be set.", {
+test_that("Check `set to` statement plus retrieval.", {
   
   # Add text
   event$text <- "<> set home a to b"
@@ -36,10 +36,31 @@ test_that("Make sure favourites can be set.", {
   expect_equal( results$startStation, "a" )
   expect_equal( results$stopStation, "b" )
   expect_null( results$updateAction )
-  
-  # Check keyword has been set in redis
   expect_equal( paste0(event$team, ":", event$user, ":home") %>% dbr$GET(), "a:b" )
   
+  # Retrieve favourite
+  event$text <- "<> home"
+  results <- event %>% 
+    translink.bot::parse_message(
+      dbr = dbr
+    )
+  
+  expect_equal( results$startStation, "a" )
+  expect_equal( results$stopStation, "b" )
+  expect_null( results$updateAction )
+
+  # Retrieve favourite (Reversed)
+  event$text <- "<> home'"
+  results <- event %>% 
+    translink.bot::parse_message(
+      dbr = dbr
+    )
+  
+  expect_equal( results$startStation, "b" )
+  expect_equal( results$stopStation, "a" )
+  expect_null( results$updateAction )
+
+  # Reset database
   dbr$FLUSHDB()
 
 })
