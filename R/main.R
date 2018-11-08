e7fbd15a8fd5d54e1cc1997c8ac4c104977a9954
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-08 11:38:50 +0000

    Add a request blocker in (for multi-similar requests and responses...)

--- R/main.R
+++ R/main.R
@@ -13,78 +13,94 @@ main <- function(req, logger = FALSE) {
   # Append the teamID onto the event!
   allInfo$event$team <- allInfo$team_id
   
-  # Parse the input & get start and stop stations
-  if (logger) cat(crayon::green(" | Parsing message \n"))
-  myStations <- allInfo$event %>% 
-    translink.bot::parse_message(
-      dbr = req$dbr
-  )
-  
-  # If station information exists, report back
-  if (myStations$updateAction %>% is.null) {
-    startStation <- myStations$startSt
-    stopStation <- myStations$stopSt
+  # Block request if necessary
+  blockName <- paste0(allInfo$event$channel, allInfo$event$user, allInfo$event$timestamp)
+
+  # Check the request isn't blocked
+  result <- blockName %>% 
+    req$dbr$GET(key = blockName)
+
+  if (result %>% is.null) {
+    # Set a time-out on the particular request (10 minutes)
+    blockName %>% req$dbr$SET(
+      value = "blocked",
+      EX = 60 * 10
+    )
     
-    # Load up station list
-    if (logger) cat(crayon::green(" | Getting available stations \n"))
-    station.list <- translink.bot::get_stations()
+    # Parse the input & get start and stop stations
+    if (logger) cat(crayon::green(" | Parsing message \n"))
+    myStations <- allInfo$event %>% 
+      translink.bot::parse_message(
+        dbr = req$dbr
+    )
     
-    # Lower case everything
-    startStation %<>% tolower %>% Hmisc::upFirst()
-    stopStation %<>% tolower %>% Hmisc::upFirst()
-    
-    # Get the start code
-    if (logger) cat(crayon::green(" | Checking input against station list \n"))
-    startCode <- station.list$code %>% 
-      `[`(startStation %>% 
-            stringdist::stringdist(station.list$name, method = 'jw') %>%
-            which.min
-         )
-    
-    # Get calling information
-    if (logger) cat(crayon::green(" | Sending query \n"))
-    allresults <- startCode %>% 
-      translink.bot::query_live()
-    
-    # Need to make sure that the start and stop stations are actually close
-    # to something at all...
-    
-    
-    # Check the calling points, i.e. get ids in the right direction
-    if (logger) cat(crayon::green(" | Parsing calling points \n"))
-    correctWay <- lapply(
-      X = allresults$callingpoints,
-      FUN = function(x) if (stopStation %in% (x %>% `[[`("Name") %>% as.character)) T else F 
-    ) %>% 
-      purrr::flatten_lgl()
-    
-    # Subset the right way details
-    allresults$callingpoints %<>% `[`(correctWay)
-    allresults$myresults %<>% subset(correctWay)
-    
-    slacktext <- allresults$myresults %>% 
-      translink.bot::create_text(
-        startStation = startStation,
-        stopStation = stopStation
+    # If station information exists, report back
+    if (myStations$updateAction %>% is.null) {
+      startStation <- myStations$startSt
+      stopStation <- myStations$stopSt
+      
+      # Load up station list
+      if (logger) cat(crayon::green(" | Getting available stations \n"))
+      station.list <- translink.bot::get_stations()
+      
+      # Lower case everything
+      startStation %<>% tolower %>% Hmisc::upFirst()
+      stopStation %<>% tolower %>% Hmisc::upFirst()
+      
+      # Get the start code
+      if (logger) cat(crayon::green(" | Checking input against station list \n"))
+      startCode <- station.list$code %>% 
+        `[`(startStation %>% 
+              stringdist::stringdist(station.list$name, method = 'jw') %>%
+              which.min
+           )
+      
+      # Get calling information
+      if (logger) cat(crayon::green(" | Sending query \n"))
+      allresults <- startCode %>% 
+        translink.bot::query_live()
+      
+      # Need to make sure that the start and stop stations are actually close
+      # to something at all...
+      
+      
+      # Check the calling points, i.e. get ids in the right direction
+      if (logger) cat(crayon::green(" | Parsing calling points \n"))
+      correctWay <- lapply(
+        X = allresults$callingpoints,
+        FUN = function(x) if (stopStation %in% (x %>% `[[`("Name") %>% as.character)) T else F 
+      ) %>% 
+        purrr::flatten_lgl()
+      
+      # Subset the right way details
+      allresults$callingpoints %<>% `[`(correctWay)
+      allresults$myresults %<>% subset(correctWay)
+      
+      slacktext <- allresults$myresults %>% 
+        translink.bot::create_text(
+          startStation = startStation,
+          stopStation = stopStation
+        )
+      
+      mybody <- list(
+        token = Sys.getenv("SLACK_TOKEN"), 
+        text = slacktext,
+        channel = allInfo$event$channel,
+        color = "#009990"
       )
-    
-    mybody <- list(
-      token = Sys.getenv("SLACK_TOKEN"), 
-      text = slacktext,
-      channel = allInfo$event$channel,
+    } else {
+      mybody <- list(
+        token = Sys.getenv("SLACK_TOKEN"), 
+        text = myStations$updateAction,
+        channel = allInfo$event$channel
+      )
+    }
+
+    # Simple post
+    res <- httr::POST(
+      url = "https://slack.com/api/chat.postMessage", 
+      body = mybody,
       color = "#009990"
     )
-  } else {
-    mybody <- list(
-      token = Sys.getenv("SLACK_TOKEN"), 
-      text = myStations$updateAction,
-      channel = allInfo$event$channel
-    )
   }
-  # Simple post
-  res <- httr::POST(
-    url = "https://slack.com/api/chat.postMessage", 
-    body = mybody,
-    color = "#009990"
-  )
 }
