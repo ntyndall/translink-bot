68c5b343a564ae04cc7a7b2885979584be2cc855
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-08 16:10:44 +0000

    Updating function for /tdelete

--- R/parse_delete.R
+++ R/parse_delete.R
@@ -1,48 +1,65 @@
-#' @title Parse Info
-#'
+#' @title Parse Delete Favourites
+#' 
 #' @export
 
 
-parse_delete <- function(dbr, allFavs, myMessage) {
+parse_delete <- function(dbr, event) {
   
-  # Set up pipeline
-  redpipe <- redux::redis
+  myMessage <- event$text
+
+  # Look up ALL favourites
+  allFavs <- paste0(event$team_id, ":", event$user_id, "*") %>% 
+    dbr$KEYS()
   
-  # Get all keywords
-  allkwords <- allFavs %>% 
-    strsplit(split = ":") %>% 
-    purrr::map(3) %>% 
-    purrr::flatten_chr()
-  
-  kword <- translink.bot::which_tag(myMessage, "delete")
-  
-  if (kword == "all") {
-    results <- dbr$pipeline(
-      .commands = lapply(
-        X = allFavs,
-        FUN = function(x) x %>% redpipe$DEL()
-      )
-    )
-    favnames <- allkwords
-  } else if (kword %in% allkwords) {
-    # Which one?
-    results <- allFavs[kword %>% `==`(allkwords) %>% which] %>% dbr$DEL() 
-    favnames <- kword
+  # Flatten list of favourites
+  if (allFavs %>% length %>% `>`(0)) {
+    allFavs %<>% purrr::flatten_chr()
+    
+    # Set up pipeline
+    redpipe <- redux::redis
+    
+    # Get all keywords
+    allkwords <- allFavs %>% 
+      strsplit(split = ":") %>% 
+      purrr::map(3) %>% 
+      purrr::flatten_chr()
+    
+    # Get all keywords
+    kwords <- myMessage
+    
+    # Check intersection
+    multikw <- kwords %>% intersect(allkwords)
+    
+    if ("all" %in% kwords) {
+      results <- dbr$pipeline(
+        .commands = lapply(
+          X = allFavs,
+          FUN = function(x) x %>% redpipe$DEL()
+        )
+      ) %>%
+        purrr::flatten_chr()
+      
+      favnames <- allkwords
+    } else if (multikw %>% length %>% `>`(0)) {
+      # Which one? match them up first
+      results <- dbr$pipeline(
+        .commands = lapply(
+          X = allFavs[multikw %>% match(allkwords)],
+          FUN = function(x) x %>% redpipe$DEL()
+        )
+      ) %>%
+        purrr::flatten_chr()
+      
+      favnames <- multikw
+    } else {
+      return(paste0("Nothing to delete for the supplied arguments"))
+    }
+    
+    slackTxt <- paste0("Deleting routes: \n- ", favnames %>% paste(collapse = "\n- "))
   } else {
-    return(
-      list(
-        startSt = NULL,
-        stopSt = NULL,
-        updateAction = paste0("Nothing to delete for : ", kword)
-      )
-    )
+    slackTxt <- "You do not have any routes to delete"
   }
   
-  return(
-    list(
-      startSt = NULL,
-      stopSt = NULL,
-      updateAction = paste0("Deleting information ... \n- ", favnames %>% paste(collapse = "\n- "))
-    )
-  )
+  # Return string back to slack
+  return(slackTxt)
 }
