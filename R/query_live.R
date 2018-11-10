dfb8df82a88c15aac4e74221f32b15e3538e53f4
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-10 00:06:53 +0000

    Make sure empty nodeset is handled.

--- R/query_live.R
+++ R/query_live.R
@@ -14,39 +14,45 @@ query_live <- function(stationCode) {
     xml2::read_xml() %>%
     xml2::xml_find_all(".//Service")
   
-  # Get calling points as a list of data frames
-  callingpoints <- lapply(
-    X = services, 
-    FUN = function(x) x %>% xml2::xml_find_all(".//CallingPoint") %>% xml2::as_list() %>% lapply(attributes)
-  ) %>% lapply(
-    FUN = function(x) {
-      if (x %>% length %>% `!=`(0)) {
-        x %>% lapply(as.data.frame) %>% purrr::reduce(rbind) 
-      } else {
-        NULL
+  # Make sure the trains are still running
+  if (services %>% length %>% `>`(0)) {
+    # Get calling points as a list of data frames
+    callingpoints <- lapply(
+      X = services, 
+      FUN = function(x) x %>% xml2::xml_find_all(".//CallingPoint") %>% xml2::as_list() %>% lapply(attributes)
+    ) %>% lapply(
+      FUN = function(x) {
+        if (x %>% length %>% `!=`(0)) {
+          x %>% lapply(as.data.frame) %>% purrr::reduce(rbind) 
+        } else {
+          NULL
+        }
       }
-    }
-  )
-  
-  # Get the remainder of tags
-  rest <- lapply(
-    X = ".//" %>% paste0(translink.bot::allowed_tags()),
-    FUN = function(y) services %>% xml2::xml_find_first(y) %>% xml2::as_list() %>% lapply(attributes)
-  )
-  
-  # Now convert to a data frame
-  myresults <- lapply(
-    X = rest,
-    FUN = function(x) x %>% lapply(as.data.frame) %>% purrr::reduce(rbind)
-  )
-  
-  # Manually update these for now
-  names(myresults[[2]]) <- c("arrivetime", "arrived", "arrivedtstamp")
-  names(myresults[[7]]) <- c("originname", "origintiploc", "origincrs")
-  
-  # Create data frame
-  myresults %<>% purrr::reduce(cbind)
-  
+    )
+    
+    # Get the remainder of tags
+    rest <- lapply(
+      X = ".//" %>% paste0(translink.bot::allowed_tags()),
+      FUN = function(y) services %>% xml2::xml_find_first(y) %>% xml2::as_list() %>% lapply(attributes)
+    )
+    
+    # Now convert to a data frame
+    myresults <- lapply(
+      X = rest,
+      FUN = function(x) x %>% lapply(as.data.frame) %>% purrr::reduce(rbind)
+    )
+    
+    # Manually update these for now
+    names(myresults[[2]]) <- c("arrivetime", "arrived", "arrivedtstamp")
+    names(myresults[[7]]) <- c("originname", "origintiploc", "origincrs")
+    
+    # Create data frame
+    myresults %<>% purrr::reduce(cbind)
+  } else  {
+    return(list(myresults = NULL, callingPoints = NULL))
+  }
+
+  # Return results back
   return(
     list(
       myresults = myresults,
