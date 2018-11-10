6882a1d67fba2eee32df55a5e8b7c8750399c91d
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-10 15:53:30 +0000

    Set up functionality for parsing train lines!

---
+++ R/parse_lines.R
@@ -0,0 +1,14 @@
+#' @title Parse Lines
+#' 
+#' @export
+
+
+parse_lines <- function(dbr, event) {
+  
+  # Get the actual message
+  myMessage <- event$text
+  
+  # Parse the lines
+  # ...
+  
+}
