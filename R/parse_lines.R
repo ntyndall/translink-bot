0f0ba4c72a6ebf41e67cca22b7410d38cd8153a2
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-11-12 14:09:42 +0000

    return station lines back through /tlines

--- R/parse_lines.R
+++ R/parse_lines.R
@@ -3,12 +3,28 @@
 #' @export
 
 
-parse_lines <- function(dbr, event) {
+parse_lines <- function(dbr) {
   
   # Get the actual message
   myMessage <- event$text
   
   # Parse the lines
-  # ...
+  results <- "stationlines" %>% 
+    dbr$HGETALL()
   
+  results %>% `[`(c(T, F)) %>% purrr::flatten_chr()
+  mylines <- results %>% `[`(c(F, T)) %>% purrr::flatten_chr()
+
+  # Return the lines back
+  return(
+    list(
+      attachments = data.frame(
+        pretext = "Listing available train lines + colours;",
+        fallback = paste0(" - ",  mylines),
+        text = mylines,
+        color = results %>% `[`(c(T, F)) %>% purrr::flatten_chr()
+      )
+    ) %>% 
+      jsonlite::toJSON()
+  )
 }
